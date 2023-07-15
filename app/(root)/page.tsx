bda129bd4b3b808d22f16e070f7b8ff99e0c9805
iNFiENiTE-World <infienite@gmail.com>
2023-07-15 12:27:51 +0800

    Include Clerk user management button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <p>Hello Admin Dashboard</p>
-      <Button size="default" variant="destructive">Click ME!</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
+
+export default SetupPage
