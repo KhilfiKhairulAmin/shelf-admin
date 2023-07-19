ce0eba11ee2bb56ca047ceb49caba6c2ad21229a
iNFiENiTE-World <infienite@gmail.com>
2023-07-19 20:05:48 +0800

    Add POST /api/stores

---
+++ app/api/stores/route.ts
@@ -0,0 +1,37 @@
+import prismadb from "@/lib/prismadb"
+
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+export async function POST (
+  req: Request,
+) {
+  try {
+    // Authentication
+    const { userId } = auth()
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const { name } = await req.json()
+
+    // Data validation
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 })
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId
+      }
+    })
+
+    return NextResponse.json(store)
+    
+  } catch (error) {
+    console.log('[STORES_POST]', error)
+    return new NextResponse('Internal error', { status: 500 })
+  }
+}
