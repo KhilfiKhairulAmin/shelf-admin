261e1f0f2cac5b3ce07dfb5603b4c6338b341542
iNFiENiTE-World <infienite@gmail.com>
2023-07-23 12:03:54 +0800

    Fix UI position bug

--- components/ui/ApiAlert.tsx
+++ components/ui/ApiAlert.tsx
@@ -2,6 +2,7 @@ import { Copy, Server } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "./alert"
 import { Badge, BadgeProps } from "./badge"
 import { Button } from "./button"
+import { toast } from "react-hot-toast"
 
 interface ApiAlertProps {
   title: string
@@ -22,18 +23,18 @@ const variantMap: Record<ApiAlertProps['variant'], BadgeProps['variant']> = {
 export const ApiAlert: React.FC<ApiAlertProps> = ({
   title,
   description,
-  variant
+  variant = 'public'
 }) => {
 
-  const onCopy = (description: string) => {
+  const onCopy = () => {
     navigator.clipboard.writeText(description)
-    
+    toast.success('API Route copied to the clipboard')
   }
 
   return (
     <Alert>
-      <Server className="h-4 w-4" />
       <AlertTitle className="flex items-center gap-x-2">
+        <Server className="h-4 w-4" />
         {title}
         <Badge variant={variantMap[variant]}>
           {textMap[variant]}
@@ -43,7 +44,7 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
           {description}
         </code>
-        <Button variant="outline" size="icon" onClick={() => {}}>
+        <Button variant="outline" size="icon" onClick={onCopy}>
           <Copy className="h-4 w-4" />
         </Button>
       </AlertDescription>
